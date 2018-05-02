9cdd8f7f40d77a61411f41f40e6c58f6e596f00e
redgemini <wkiddy@gmail.com>
2018-05-02 18:34:37 -0500

    update config files

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+console.log ("load connection.js test");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
