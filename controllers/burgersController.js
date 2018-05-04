776a34b6cb9713b1bf3dd55543257202b0dfa91f
redgemini <wkiddy@gmail.com>
2018-05-04 11:13:28 -0500

    updateViews

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -16,29 +16,34 @@ router.get("/burgers", function(req, res) {
     
   // express callback response by calling burger.selectAllBurger
   burger.all(function(burgerData) {
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+
+    //MySQL query callback will return burger_data
     res.render("index", { burger_data: burgerData });
+
   });
 });
 
-// post route -> back to index
+// post route 
 router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for buger.addBurger
+
+  // takes the request for burger.addBurger
   burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
+
+    //callback will return a log to console,
     console.log(result);
     res.redirect("/");
+
   });
 });
 
-// put route -> back to index
+// put route - back to index
 router.put("/burgers/:id", function(req, res) {
   burger.update(req.params.id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
+
+    // console.log db update callback
     console.log(result);
-    // Send back response and let page reload from .then in Ajax
+
+    // Send back response
     res.sendStatus(200);
   });
 });
