50a18527000d60bc872711f100421aff28abf5d8
redgemini <wkiddy@gmail.com>
2018-05-07 16:36:48 -0500

    updateDB

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hickory Burger', FALSE);
