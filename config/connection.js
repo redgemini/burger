459862488f65ee711a79c4d0367d61c83f71e276
redgemini <wkiddy@gmail.com>
2018-05-07 08:17:47 -0500

    updateConnection.JS

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,7 @@ if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWDB_URL);
 } else {
   connection = mysql.createConnection({
+  port: 3306,
   host: "localhost",
   user: "root",
   password: "",
